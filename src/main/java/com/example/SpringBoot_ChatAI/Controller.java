25cd649acbb42f4014006929f0bcbcdcdcd2d418
mohitsinghnegi10 <mohitsingh10799@gmail.com>
2024-10-29 19:50:59 +0530

    add prompt template

--- src/main/java/com/example/SpringBoot_ChatAI/Controller.java
+++ src/main/java/com/example/SpringBoot_ChatAI/Controller.java
@@ -1,9 +1,14 @@
 package com.example.SpringBoot_ChatAI;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatPromptTemplate;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class Controller {
@@ -16,7 +21,12 @@ public class Controller {
 
 
     @GetMapping("/")
-    public String prompt(@RequestParam String msg) {
-        return chatModel.call(msg);
+    public String prompt(@RequestParam(defaultValue = "Hi") String msg) {
+        String message = """
+                give only 3 {v} name
+                """;
+        PromptTemplate temp = new PromptTemplate(message);
+        Prompt prompt = temp.create(Map.of("v",msg));
+        return chatModel.call(prompt.getContents());
     }
 }
